55ad8042550322b1fd560dc453ffcb19d9ab3008
iamwill <19693587+iamwillzhu@users.noreply.github.com>
2020-05-07 17:35:54 -0400

    added qu character symbol png file

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-import {qulinks_url} from "qulinks_url";
 chrome.omnibox.onInputEntered.addListener(function(text) {
     const url = qulinks_url + text;
+    console.log(url)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.update(tabs[0].id, {url: url});
     });
